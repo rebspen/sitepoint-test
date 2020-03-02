c672fe8292e61dc330661010fb3fda3782fbb040
Rebspen <rebeccaspence111@gmail.com>
2020-03-02 15:00:43 +0000

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
+import  "./../styles/global.css"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import  FancyParagraph  from  "../components/fancy-paragraph"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to my SitePoint Demo Site!</p>
+    <FancyParagraph  paragraphText="Styled with CSS Modules." />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
